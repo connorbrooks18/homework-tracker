3017445299ce7d771c444cbb290c2f881d4595dd
human-who-codes <connorbrooks18@icloud.com>
2022-01-07 13:52:56 -0600

    Added a python file for hosting index.html on wifi

---
+++ lanhosting.py
@@ -0,0 +1,29 @@
+# serve this directory except for some files
+# in the same directory as this script
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+    
+class handler(BaseHTTPRequestHandler):
+    # serve all the index.html, script.js, and style.css files
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        try:
+            # check if the file exists
+            f = open(self.path[1:]).read()
+            self.send_response(200)
+        except:
+            # file does not exist, return 404
+            f = 'file not found'
+            self.send_response(404)
+        self.end_headers()
+        self.wfile.write(bytes(f, 'utf-8'))
+        
+
+def main():
+    server = HTTPServer(('192.168.4.42', 8000), handler)
+    print('Starting server, use <Ctrl-C> to stop')
+    server.serve_forever()
+
+if "__main__" == __name__:
+    main()
